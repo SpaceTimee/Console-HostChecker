ae0d018e0ac7e1f2bdbcffc2b3eb245228712c83
Space Time <Zeus6_6@163.com>
2024-09-06 11:18:05 +0800

    1.1.1.3 更新

--- Console-HostChecker.ps1
+++ Console-HostChecker.ps1
@@ -1,17 +1,21 @@
-﻿param ([switch] $trans)
+﻿param ([switch] $trans, [switch] $loop)
 
-if ($trans) { Start-Transcript (Join-Path $PSScriptRoot "Trans.log") -UseMinimalHeader | Out-Null }
+do {
+    if ($trans) { Start-Transcript (Join-Path $PSScriptRoot "Trans.log") -UseMinimalHeader | Out-Null }
 
-if ($PSEdition -ne "Core") {
-    Write-Host "Console HostChecker 需要在 PowerShell 7.x 环境运行"
-}
-else {
-    foreach ($ps1File in Get-ChildItem $PSScriptRoot "*.ps1") {
-        if ($ps1File.FullName -ne $PSCommandPath) { . $ps1File.FullName }
+    if ($PSEdition -ne "Core") {
+        Write-Host "Console HostChecker 需要在 PowerShell 7.x 环境运行"
+    }
+    else {
+        foreach ($ps1File in Get-ChildItem $PSScriptRoot "*.ps1") {
+            if ($ps1File.FullName -ne $PSCommandPath) { . $ps1File.FullName }
+        }
+
+        [App]::new().Main()
     }
 
-    [App]::new().Main()
-}
+    if ($trans) { Stop-Transcript }
+    if ($loop) { Start-Sleep 60 }
+} while ($loop)
 
-if ($trans) { Stop-Transcript }
-else { Read-Host "按回车键结束" }
+Read-Host "按回车键结束"
